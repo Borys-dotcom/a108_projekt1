acecd9a0c0e35318c9ecda7af7fa9cd0f83faf2d
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-11-26 13:47:05 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.hamburger-icon').addEventListener("click", () => {
+    document.querySelector('.actual-menu').classList.toggle('visibility');
+    document.querySelector('.hamburger-icon').classList.toggle('visibility');
+});
+
+document.querySelector('.menu-close-icon').addEventListener("click", () => {
+    document.querySelector('.actual-menu').classList.toggle('visibility');
+    document.querySelector('.hamburger-icon').classList.toggle('visibility');
+});
