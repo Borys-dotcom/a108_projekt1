33320c9e2d20a4e10bcb1dd160dad1c6f2f53eef
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-11-27 13:45:05 +0100

    version 0.3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 document.querySelector('.hamburger-icon').addEventListener("click", () => {
     document.querySelector('.actual-menu').classList.toggle('visibility');
-    document.querySelector('.hamburger-icon').classList.toggle('visibility');
 });
 
 document.querySelector('.menu-close-icon').addEventListener("click", () => {
     document.querySelector('.actual-menu').classList.toggle('visibility');
-    document.querySelector('.hamburger-icon').classList.toggle('visibility');
 });
